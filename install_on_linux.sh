e3342e227eedfd079dffe3696a70a1111f040c81
Swaraj Puppalwar <79976106+UltronTheAI@users.noreply.github.com>
2023-03-08 19:57:08 +0530

    Create install_on_linux.sh

---
+++ install_on_linux.sh
@@ -0,0 +1,14 @@
+# pip3 install typer
+pip3 install typing
+pip3 install colorama
+pip3 install prettytable
+pip3 install secrets
+pip3 install pyinstaller
+git clone "https://github.com/UltronTheAI/productivePy.git"
+pyinstaller -F ./productivePy/productivePy.py
+chmod +x ./dist/productivePy
+mkdir -p ~/bin/productivePy
+cp ./dist/productivePy ~/bin/productivePy
+echo 'export PATH="$HOME/bin/productivePy:$PATH"' >> ~/.zshrc
+source ~/.zshrc
+echo "Installation completed!"
