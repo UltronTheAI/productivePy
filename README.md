b881c3789c6d6346d0592603c7e0103b45ebbbfd
Swaraj Puppalwar <79976106+UltronTheAI@users.noreply.github.com>
2023-03-08 11:11:35 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,93 @@
+# ProductivePy
+
+ProductivePy is a productivity tool for Python developers that helps automate repetitive tasks and saves time.
+
+## Installation
+
+To install ProductivePy, run the following command:
+- Install required modules.
+```sh
+$ pip3 install typer typing colorama prettytable secrets
+```
+- Clone the productivePy Github repository.
+```bash
+$ git clone $url
+```
+- Copy the productivePy.py file from ./productivePy/productivePy.py in to your project.
+```bash
+$ cp ./productivePy/productivePy.py "path to your project"
+```
+- Go to your project.
+```bash
+$ cd "Your project"
+```
+- Create new python file, and run it with ProductivePy.
+```bash
+$ python3 productivePy.py run $fileName
+```
+
+## Commands
+
+ProductivePy supports the following commands:
+
+- `run`: Run a Python file
+- `re-run`: Re-run the last Python file that was run
+- `status`: Show the status of running tasks
+- `stop`: Stop a running task
+- `stopAll`: Stop all running tasks
+
+## Options
+
+ProductivePy supports the following options:
+
+- `--version`: Get the version of ProductivePy
+- `--dealy`: Set a timeout for running a file after updating
+- `--args`: Add arguments to the command used to run the file
+- `--clear`: Clear the screen before re-running a file
+- `--autostart`: Auto-restart a file after it ends
+
+# Command and Options, Examples
+- To run a python file.
+```bash
+$ python3 productivePy.py run $fileName
+```
+- Status
+```bash
+$ python3 productivePy.py status
+```
+- Re-run, you can get id, by running status command, or searching id from productivePy.json, file in your project dir.
+```bash
+$ python3 productivePy.py re-run $id
+```
+- Stop, this command takes id, to stop the running process.
+```bash
+$ python3 productivePy.py stop $id
+```
+- stopAll, this command will stop all running processes.
+```bash
+$ python3 productivePy.py stopAll
+```
+- `--clear` argument, this argument will clear your screen after your code re-runs, when updated.
+```bash
+$ python3 productivePy.py run $fileName --clear
+```
+- `--dealy`, this argument set up the dealy, between shifting from running file to re-run it.
+```bash
+$ python3 productivePy.py run $fileName --dealy 4
+```
+- `--autostart`, this argument, autostart's your running process, when it is finished.
+```bash
+$ python3 productivePy.py run $fileName --autostart
+```
+- `--args`, this argument add the extra args, => `python3 $fileName $args`
+```bash
+$ python3 productivePy.py run $fileName --args $args
+```
+## Authors
+
+- [@UltronTheAI](https://github.com/UltronTheAI)
+
+
+## License
+
+ProductivePy is released under the MIT License. See `LICENSE` for more information.
---
+++ productivePy.py
@@ -0,0 +1,193 @@
+from typer import *  # Import typer.
+from typing import *  # Import typing.
+from prettytable import PrettyTable
+import colorama  # Import the colorama.
+import secrets
+import time
+import glob
+import json
+import subprocess
+import os as cmd
+import platform
+
+colorama.init()  # Initialize colorama.
+app = Typer()  # Initialize application.
+
+config = {
+    "config": {},
+    "running": [],
+    "ids": []
+}
+
+os = platform.system()
+python = 'python' if os == 'Windows' else 'python3'
+
+
+class Config:  # Configuration of the application.
+    files = glob.glob('*')
+
+    def __init__(self):  # Initializes the configuration.
+        global config
+        if 'productivePy.json' in self.files:
+            with open('productivePy.json', 'r') as f:
+                config = json.load(f)
+                f.close()
+        else:
+            with open('productivePy.json', 'w') as f:
+                f.write('{\n\t"config": {},\n\t"running": [],\n\t"ids": []\n}')
+                f.close()
+
+    def update(self, data):
+        try:
+            with open('productivePy.json', 'w') as f:
+                f.write(json.dumps(data, indent=4))
+                f.close()
+        except:
+            pass
+
+    def get(self):
+        data = None
+        while True:
+            try:
+                with open('productivePy.json', 'r') as f:
+                    data = json.load(f)
+                    f.close()
+                break
+            except:
+                pass
+        return data
+
+
+setup = Config()  # Config.
+
+
+def cls():
+    global os
+    cmd.system(f'cls' if os == 'Windows' else f'clear')
+    return
+
+
+@app.command()
+# Main entry point.
+def main(
+        version: Optional[bool] = Option(None, metavar='version',
+                                         show_default=False, help='Get version of productivePy.'),
+        command: Optional[str] = Argument(
+            None, show_default=False, metavar='[ run/re-run/status/stop/stopAll ]', rich_help_panel='Commands', help='Commands.'),
+        file: Optional[str] = Argument(None, show_default=False, rich_help_panel='Commands',
+                                       metavar='file', help='Config file, to perform tasks.'),
+        dealy: Optional[int] = Option(0, show_default=False, metavar='time',
+                                      rich_help_panel='Commands', help='Time out for running file after updating.'),
+        args: Optional[str] = Option('', show_default=False, metavar='args',
+                                     rich_help_panel='Commands', help='Add args => \'$ python $file $args\''),
+        clear: Optional[bool] = Option(0, show_default=False, metavar='clear-screen',
+                                       rich_help_panel='Commands', help='Clear screen before re-run.'),
+        autoStart: Optional[bool] = Option(0, show_default=False, metavar='auto-restart',
+                                       rich_help_panel='Commands', help='Auto restart after program ende`s.'),
+):
+    global config, os, python
+    if version:
+        print('1.0.0')
+        raise Exit()
+    elif command:
+        if command in ['run', 're-run']:
+            if file:
+                file = file if '.py' in file else config['config'][file][0]
+                id = secrets.token_hex(5) if '.py' in file else file
+                config['running'].append(str(id))
+                config['ids'].append(str(id))
+                config['config'][str(id)] = [file, 'running']
+                setup.update(config)
+                print(f'{colorama.Fore.LIGHTYELLOW_EX}[productivePy] 1.0.0')
+                print(
+                    f'{colorama.Fore.LIGHTYELLOW_EX}[productivePy] to restart at any time, fire `productivePy re-run $id` command in terminal')
+                print(
+                    f'{colorama.Fore.LIGHTYELLOW_EX}[productivePy] watching path(s): *.*')
+                print(
+                    f'{colorama.Fore.LIGHTYELLOW_EX}[productivePy] watching extensions: py')
+                print(
+                    f'{colorama.Fore.LIGHTGREEN_EX}[productivePy] starting ` {python} "{file}" `{colorama.Fore.WHITE}')
+                try:
+                    exe = subprocess.Popen([python, file, args])
+                    code = open(file, 'r').read()
+                    repeat = 0
+                    rs = 0
+
+                    while id in config['running']:
+                        time.sleep(dealy)
+                        newCode = open(file, 'r').read()
+                        config = setup.get()
+
+                        if code != newCode:
+                            code = newCode
+                            del newCode
+
+                            if clear:
+                                cls()
+                            if repeat == 0:
+                                repeat += 1
+                            else:
+                                repeat -= 1
+                                rs -= 1
+                                print(
+                                    f'{colorama.Fore.LIGHTGREEN_EX}[productivePy] restarting due to changes...')
+                                print(
+                                    f'{colorama.Fore.LIGHTGREEN_EX}[productivePy] starting ` {python} "{file}" `{colorama.Fore.LIGHTWHITE_EX}')
+                                exe.terminate()
+                                exe = subprocess.Popen([python, file, args])
+                        elif exe.poll() == 0 and rs == 0:
+                            rs += 1
+                            print(
+                                f'{colorama.Fore.LIGHTGREEN_EX}[productivePy] clean exit - waiting for changes before restart...')
+                            if autoStart:
+                                repeat -= 1
+                                rs -= 1
+                                print(
+                                    f'{colorama.Fore.LIGHTGREEN_EX}[productivePy] restarting due to changes...')
+                                print(
+                                    f'{colorama.Fore.LIGHTGREEN_EX}[productivePy] starting ` {python} "{file}" `{colorama.Fore.LIGHTWHITE_EX}')
+                                exe.terminate()
+                                exe = subprocess.Popen([python, file, args])
+                        else:
+                            pass
+
+                    print(
+                        f'{colorama.Fore.LIGHTGREEN_EX}[productivePy] clean exit...')
+                except:
+                    config['running'].remove(id)
+                    config['config'][id] = [file, 'stoped']
+                    setup.update(config)
+                    print(
+                        f'{colorama.Fore.LIGHTGREEN_EX}[productivePy] clean exit...')
+
+            else:
+                raise Exception("Invalid file.")
+        elif command == 'status':
+            if clear:
+                config = {
+                    "config": {},
+                    "running": [],
+                    "ids": []
+                }
+                setup.update(config)
+            else:
+                table = PrettyTable()
+                table.field_names = ['No.', 'ID', 'File', 'Status']
+                no = 0
+                for out in config['ids']:
+                    no += 1
+                    table.add_row([no, out, config['config'][out]
+                                   [0], config['config'][out][1]])
+                print(table)
+        elif command == 'stop':
+            config['running'].remove(file)
+            setup.update(config)
+        elif command == 'stopAll':
+            config['running'] = []
+            setup.update(config)
+        else:
+            raise Exception("Invalid command.")
+
+
+if __name__ == '__main__':  # Initialize.
+    app()  # Initialize the application.
